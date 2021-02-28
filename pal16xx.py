f529d30c4d1c64f0a4016479bb56ea9bd0881391
John McMaster <johndmcmaster@gmail.com>
2021-02-28 10:31:59 -0800

    pal16l8: support i/o
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- pal16xx.py
+++ pal16xx.py
@@ -51,24 +51,45 @@ def add_open_states(open, tests):
 """
 
 class PAL16XXReader:
-    def __init__(self, tl, part):
+    def __init__(self, tl, part, input_pins=[]):
         assert part in ("PAL16L8", "PAL16R4", "PAL16R6", "PAL16R8")
         self.part = part
 
         if self.part == "PAL16L8":
             self.P_CLK = None
             self.P_OEn = None
-            self.A_LINES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+            self.I_LINES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+            # 13-18 are I/O
+            self.O_LINES = [12, 19]
+            self.IO_LINES = []
+            # Manually specify input pins
+            if input_pins is not None:
+                for pin in input_pins:
+                    assert 13 <= pin <= 18
+                for pin in range(13, 19):
+                    if pin in input_pins:
+                        self.I_LINES.append(pin)
+                    else:
+                        self.O_LINES.append(pin)
+            # Otherwise try to guess t
+            else:
+                self.IO_LINES = [13, 14, 15, 16, 17, 18]
         else:
             self.P_CLK = 1
             self.P_OEn = 11
             # 15 bit address => A14 max
             # 27C128
-            self.A_LINES = [2, 3, 4, 5, 6, 7, 8, 9]
-        self.WORDS = 1 << len(self.A_LINES)
+            self.I_LINES = [2, 3, 4, 5, 6, 7, 8, 9]
+            # LSB to MSB
+            self.O_LINES = [12, 13, 14, 15, 16, 17, 18, 19]
+        self.I_LINES_base = sorted(self.I_LINES)
+        self.O_LINES_base = sorted(self.O_LINES)
+        self.I_LINES = self.I_LINES_base
+        self.O_LINES = self.O_LINES_base
+        print("I_LINES: %s" % (self.I_LINES,))
+        print("O_LINES: %s" % (self.O_LINES,))
+        print("IO_LINES: %s" % (self.IO_LINES,))
         
-        # LSB to MSB
-        self.D_LINES = [12, 13, 14, 15, 16, 17, 18, 19]
         self.P_VCC = 20
         self.P_GND = 10
 
@@ -84,7 +105,7 @@ class PAL16XXReader:
         """
     
         ret = []
-        for biti, pin in enumerate(self.A_LINES):
+        for biti, pin in enumerate(self.I_LINES):
             if addr & (1 << biti):
                 ret.append(pin)
     
@@ -96,7 +117,7 @@ class PAL16XXReader:
         '''
         # LSB to MSB
         ret = 0
-        for biti, pin20 in enumerate(self.D_LINES):
+        for biti, pin20 in enumerate(self.O_LINES):
             # print("check d", zif_val, biti, pin20)
             if (1 << dip20_to_zif[pin20]) & zif_val:
                 ret |= 1 << biti
@@ -105,7 +126,7 @@ class PAL16XXReader:
         return ret
     
     def inputs_p20(self):
-        ret = self.A_LINES
+        ret = self.I_LINES
         if self.P_CLK is not None:
             ret += [self.P_CLK]
         if self.P_OEn is not None:
@@ -150,16 +171,17 @@ class PAL16XXReader:
         self.tl.gnd_pins(self.dip20s_to_zif([self.P_GND]))
         self.tl.vdd_pins(self.dip20s_to_zif([self.P_VCC]))
 
-    def sweep_combclk(self, clk):
+    def sweep_combclk_io(self, clk):
         """
         Read every address and return the resulting data
         Static value + dynamic value
         "Registers are triggered on the high going edge"
         """
-        ret = []
-        for addr in range(self.WORDS):
+        print("Solver: sweep_combclk_io()")
+        yield {"solver": "sweep_combclk_io"}
+        for addr in range(self.words()):
         # for addr in [0xCA]:
-            print("Addr 0x%04X / 0x%04X" % (addr, self.WORDS))
+            print("Addr 0x%04X / 0x%04X" % (addr, self.words()))
             # print("pass 1")
             # Clock low, OEn low
             self.tl.io_w(self.dip20s_to_zif(self.addr_to_pin20s(addr)))
@@ -174,8 +196,68 @@ class PAL16XXReader:
                 print("   clk: 0x%02X" % word_clk)
             else:
                 word_clk = None
-            ret.append((addr, word_comb, word_clk))
-        return ret
+            yield {"A": addr, "D_comb": word_comb, "D_clk": word_clk}
+
+    def setup_io(self, opins=[]):
+        self.I_LINES = list(self.I_LINES_base)
+        self.O_LINES = list(self.O_LINES_base)
+        for pin in self.IO_LINES:
+            if pin in opins:
+                self.O_LINES.append(pin)
+            else:
+                self.I_LINES.append(pin)
+        self.I_LINES = sorted(self.I_LINES)
+        self.O_LINES = sorted(self.O_LINES)
+
+    def iomap(self):
+        """
+        Return a string in pin order
+        I: input
+        O: output
+        Z: tristate (RFU)
+        P: power
+        G: ground
+        C: clock
+        0/1: fixed value
+        """
+        ret = list("?" * 20)
+        ret[self.P_VCC - 1] = "P"
+        ret[self.P_GND - 1] = "G"
+        for pin in self.I_LINES:
+            ret[pin - 1] = "I"
+        for pin in self.O_LINES:
+            ret[pin - 1] = "O"
+        if self.P_CLK is not None:
+            ret[self.P_CLK - 1] = "C"
+        if self.P_OEn is not None:
+            ret[self.P_OEn - 1] = "0"
+        assert "?" not in ret
+        return "".join(ret)
+
+    def sweep_combclk_ioio(self):
+        """
+        Read every address and return the resulting data
+        There are 6 unknown I/O lines
+        Solve these by keeping one as output each pass and treating others as inputs
+        TL866 has 1.2k resistor on ZIF that should keep chip safe w/ bus contention 
+        """
+        print("Solver: sweep_combclk_ioio()")
+        yield {"solver": "sweep_combclk_ioio"}
+        for this_opini, this_opin in enumerate(self.IO_LINES):
+            print("")
+            self.setup_io(opins=[this_opin])
+            iomap = self.iomap()
+
+            print("io %u / %u, io %s" % (this_opini + 1, len(self.IO_LINES), iomap))
+            for addr in range(self.words()):
+            # for addr in [0xCA]:
+                print("Addr 0x%04X / 0x%04X" % (addr, self.words()))
+                # print("pass 1")
+                # Clock low, OEn low
+                self.tl.io_w(self.dip20s_to_zif(self.addr_to_pin20s(addr)))
+                word_comb = self.ez2data(self.tl.io_r())
+                print("   comb: 0x%02X" % word_comb)
+                yield {"A": addr, "D_comb": word_comb, "io": iomap}
     
     def wr_clk(self, addr, clk=False):
         # clk = not clk
@@ -216,6 +298,8 @@ class PAL16XXReader:
         self.reset()
 
         if not parents:
+            print("Solver: recursive_solver()")
+            yield {"solver": "recursive"}
             # Baseline power on at address 0
             parents.append((0, self.wr_clk(0, False)))
 
@@ -227,7 +311,7 @@ class PAL16XXReader:
         if 0 and len(parents) == 1:
             itr = [0xA7]
         else:
-            itr = range(self.WORDS)
+            itr = range(self.words())
         for addr in itr:
             print("Addr 0x%02X" % (addr,))
             self.state_reset(parents)
@@ -253,7 +337,7 @@ class PAL16XXReader:
                     print("  addr 0x%02X: comb 0x%02X, clkp 0x%02X, clkn 0x%02X, change %u" % (addr, word_comb, word_clkp, word_clkn, word_comb != word_clkp))
 
                 raise Exception("Bad clock transition")
-            ret.append((addr, word_comb, word_clkp))
+            ret.append({"A": addr, "D_comb": word_comb, "D_clk": word_clkp})
             if word_clkp not in found_states:
                 found_states.add(word_clkp)
                 pending_recurse[word_clkp] = addr
@@ -263,10 +347,10 @@ class PAL16XXReader:
             print("")
             print("Recursing on %u / %u (0x%02X, 0x%02X)" % (iteri + 1, len(pending_recurse), addr, word_clk))
             child_parents = parents + [(addr, word_clk)]
-            ret += self.recursive_solver(found_states=found_states, parents=child_parents)
+            for x in self.recursive_solver(found_states=found_states, parents=child_parents):
+                yield x
             print("Returned, depth now %u" % len(parents))
 
-        return ret
 
     def is_clkless(self, words):
         """
@@ -278,15 +362,22 @@ class PAL16XXReader:
                 return False
         return True
 
+    def words(self):
+        return 1 << len(self.I_LINES)
+
     def run(self, fnout=None):
         try:
             self.tl.led(1)
             # When clockless just brute force all inputs
             if self.P_CLK is None:
-                out = self.sweep_combclk(False)
+                if len(self.IO_LINES):
+                    out = self.sweep_combclk_ioio()
+                else:
+                    out = self.sweep_combclk_io(False)
             # Otherwise explore FF states
             else:
                 out = self.recursive_solver()
+            solver_header = next(out)
             """
             # All states reached so far
             closed_list = set()
@@ -300,28 +391,30 @@ class PAL16XXReader:
                 f = open(fnout, "w")
                 jheader = {
                     "part": self.part,
-                    "address_bits": len(self.A_LINES),
-                    "address_words": 1 << len(self.A_LINES),
-                    "data_bits": len(self.D_LINES),
-                    "data_words": 1 << len(self.D_LINES),
+                    "I_bits": len(self.I_LINES),
+                    "words": self.words(),
+                    "O_bits": len(self.O_LINES),
                     "pins": {
                         "CLK": self.P_CLK,
                         "OEn": self.P_OEn,
-                        "A": self.A_LINES,
-                        "D": self.D_LINES,
+                        "I": self.I_LINES,
+                        "O": self.O_LINES,
+                        "IO": self.IO_LINES,
                         "VCC": self.P_VCC,
                         "GND": self.P_GND,
-                    }
+                    },
+                    "soliver": solver_header,
                 }
-                write_out(f, [jheader] + out)
+                write_out(f, [jheader])
+                write_out(f, out)
         finally:
             self.tl.init()
             self.tl.led(0)
     
     
-def run(port=None, part=None, fnout=None, verbose=False):
+def run(port=None, part=None, fnout=None, input_pins=[], verbose=False):
     tl = bitbang.Bitbang(port, verbose=verbose)
-    reader = PAL16XXReader(tl, part=part)
+    reader = PAL16XXReader(tl, part=part, input_pins=input_pins)
     reader.run(fnout=fnout)
 
 def main():
@@ -331,12 +424,16 @@ def main():
     parser.add_argument('--port',
                         default=util.default_port(),
                         help='Device serial port')
+    parser.add_argument("--input-pins", default=None, help="Comma separated list of I/Os as inputs")
     parser.add_argument("--verbose", action="store_true")
     parser.add_argument('--part', required=True)
     parser.add_argument("fnout", nargs='?')
     args = parser.parse_args()
 
-    run(port=args.port, part=args.part, fnout=args.fnout, verbose=args.verbose)
+    input_pins = None
+    if args.input_pins is not None:
+        input_pins = [int(x) for x in args.input_pins.split(",")]
+    run(port=args.port, part=args.part, fnout=args.fnout, input_pins=input_pins, verbose=args.verbose)
 
 
 if __name__ == "__main__":
